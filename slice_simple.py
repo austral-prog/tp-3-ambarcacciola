a0e61db333907f8f4ec6c5271763c50dadf793c2
ambarcacciola <acacciola@mail.austral.edu.ar>
2025-07-13 17:03:51 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,5 @@
 def slice_simple():
     texto = "Awesome"
-    print (texto[0:3].lower())
-    print (texto[2:5])
-    print (texto[0:5].lower() + texto[5: ].lower())
+    print(texto[0:3].lower())
+    print(texto[2:5])
+    print(texto[0:5].lower() + texto[5:].lower())
